6acdd90b10a53074e79fd6156823e6260b142c23
Michele Mendel <michelemendel@gmail.com>
2023-03-05 20:07:57 +0100

    .

--- main_test.go
+++ main_test.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	"io"
-	"net/http"
-	"strings"
-	"testing"
-)
-
 // func TestService(t *testing.T) {
 // 	port := os.Getenv("PORT")
 // 	if port == "" {
@@ -50,27 +43,3 @@ import (
 // 		t.Errorf("HTTP Response: body does not include %q", want)
 // 	}
 // }
-
-func TestRapidAPI(t *testing.T) {
-	url := "https://scout3.p.rapidapi.com/"
-	req, _ := http.NewRequest("GET", url, nil)
-	req.Header.Add("X-RapidAPI-Key", "4581c59f6cmsh545a4f1797a2204p114ca1jsn6479f6982bd9")
-	req.Header.Add("X-RapidAPI-Host", "scout3.p.rapidapi.com")
-
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		t.Errorf("http error: %v", err)
-	}
-	defer res.Body.Close()
-	body, _ := io.ReadAll(res.Body)
-
-	if res.StatusCode != 200 {
-		t.Errorf("HTTP Response: got %q, want %q", res.StatusCode, "200")
-	}
-
-	bodyStr := strings.Trim(string(body), " \n")
-
-	if bodyStr != "Hello from Scout" {
-		t.Errorf("HTTP Response: body does not include %q", "Hello from Scout")
-	}
-}
